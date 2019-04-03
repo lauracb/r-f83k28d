5f0c7b52deb9e7a358746924f56871703c69107d
lauracb <lauracadavidb@gmail.com>
2019-04-03 17:36:33 -0500

    Se agrega funcionalidad del contador

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      contador: 0
+    };
+  }
+
+  incrementador = () => {
+    this.setState({
+      contador: this.state.contador + 1
+    });
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.contador}</span>
+        <button id="inc" onClick={this.incrementador}>Incrementa</button>
       </div>
     );
   }
